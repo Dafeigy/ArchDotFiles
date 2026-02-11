a44dd523dc5ffc4340fef468edef84e9968263da
cybersh1t <1070642765@qq.com>
2026-02-11 15:15:47 +0800

    update: make some changes

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,9 @@ if status is-interactive
 # Commands to run in interactive sessions can go here
 end
 set fish_greeting
+if status is-interactive
+	abbr -a ff fastfetch
+	abbr -a l ls
+    abbr -a ll ls -alh
+    abbr -a lll ls -lh
+end
